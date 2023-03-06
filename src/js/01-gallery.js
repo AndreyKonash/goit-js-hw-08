0dd3367de4e156b7c2edcf5d8d5f49c6713f519e
AndreyKonash <konash.andrey@gmail.com>
2023-03-06 15:50:04 +0200

    task 1-3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) =>
+  `<a class='gallery__link'  href = "${original}">
+        <img 
+            class = "gallery__image"
+            src = "${preview}"
+            data-source= "${original}"
+            alt = "${description}"
+        />
+    </a>`;
+const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  aptionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import _ from 'lodash';
 
+refs = {
+  form: document.querySelector('.feedback-form'),
+  emailInput: document.querySelector('input[name="email"]'),
+  messageInput: document.querySelector('textarea[name="message"]'),
+};
+
+refs.form.addEventListener(
+  'input',
+  _.throttle(() => {
+    const data = {
+      email: refs.emailInput.value,
+      message: refs.messageInput.value,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+  }, 500)
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (data && refs.emailInput && refs.messageInput) {
+    refs.emailInput.value = data.email;
+    refs.messageInput.value = data.message;
+  }
+});
+
+refs.form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const data = {
+    email: refs.emailInput.value,
+    message: refs.messageInput.value,
+  };
+
+  localStorage.removeItem('feedback-form-state');
+  refs.emailInput.value = '';
+  refs.messageInput.value = '';
+
+  console.log(data);
+});
